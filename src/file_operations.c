6760d069c2fbe4373772c6a8446acb507873b38f
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-12-24 05:35:32 +0300

    feat: touch

--- src/file_operations.c
+++ src/file_operations.c
@@ -1,4 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "../include/logger.h"
+
 int file_touch(int argc, char *argv[]) {
+  if (argc < 2) {
+    perror("folder name must be given.");
+    return 1;
+  }
+
+  char *file_name = argv[argc - 1];
+
+  // check if file already exists
+  struct stat st;
+  if (stat(file_name, &st) == 0) {
+    perror("file already exists.");
+    return 1;
+  }
+
+  // create file with creat call
+  int fd = creat(file_name, 0644);
+  if (fd == -1) {
+    perror("file creation failed.");
+    return 1;
+  }
+
+  char *text = "file created successfully.\n";
+  printf("%s", text);
+  logD("mtouch", text);
+  close(fd);
   return 0;
 }
 
